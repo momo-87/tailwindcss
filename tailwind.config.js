8fb93a6af0f73d2de2627bcec4c2ea9f2aff1d89
Momo-87 <momochristian87@gmail.com>
2023-07-19 19:00:30 +0100

    Install and configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-primary': '#01051e',
+        'color-primary-light': '#020726',
+        'color-primary-dark': '#010417',
+        'color-secondary': '#ff7d3b',
+        'color-gray': '#333',
+        'color-white': '#fff',
+        'color-blob': '#a427df',
+      },
+    },
   },
   plugins: [],
 };
